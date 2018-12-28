fcddb04aa75da076daf420859d4ad857ea70a9aa
nobilotus <46036915+nobilotus@users.noreply.github.com>
2018-12-28 19:17:27 +0530

    Update set2.8.c

--- set2.8.c
+++ set2.8.c
@@ -1,1 +1,35 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,i,j,x,y,count,sum,total,rem;
+  clrscr();
+  scanf("%d%d",&a,&b);
+  for(i=a;i<=b;i++)
+  {
+    x=i;
+    y=i;sum=1,count=0;total=0;
+    while(x!=0)
+    {
+      count++;
+      x=x/10;
+    }
+    while(y!=0)
+    {
+      rem=y%10;
+      for(j=0;j<count;j++)
+      {
+        sum=sum*rem;
+      }
+      total=total+sum;
+      sum=1;
+      y=y/10;
+    }
+    if(total==i)
+    {
+      printf("%d",i);
+    }
+  }
+  getch();
+}
+      
