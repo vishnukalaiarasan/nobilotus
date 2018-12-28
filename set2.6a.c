b86f184c54685687f682e1fa167c638c7b76ecba
nobilotus <46036915+nobilotus@users.noreply.github.com>
2018-12-28 18:38:06 +0530

    Rename set2.6.c to set2.6a.c

---
+++ set2.6a.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,i,j,flag=0;
+ clrscr();
+ printf("enter limit");
+ scanf("%d%d",&a,&b);
+ for(i=a;i<=b;i++)
+ {
+ for(j=2;j=i/2;j++)
+ {
+ if(i%j==0)
+ {
+ flag=1;
+ break;
+ }
+ }
+ if(flag==0)
+ {
+ pritnf("\n%d",i);
+ }
+ flag=0;
+ }
+ getch();
+ }
