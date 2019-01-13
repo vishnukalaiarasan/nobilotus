44177cff234b2031a624606d209813f5b8ce4956
nobilotus <46036915+nobilotus@users.noreply.github.com>
2019-01-13 20:25:59 +0530

    Create set4.38.c

---
+++ set4.38.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+ 
+/* Function Prototype */
+void swap(int*, int *);
+ 
+void main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    swap(&num1, &num2);        
+    printf("\n %d  %d", num1, num2);
+}
+void swap(int *x, int *y)
+{
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+}
