9511ef5efe44b642d2b961c6a96c665ab01b8919
nobilotus <46036915+nobilotus@users.noreply.github.com>
2019-01-20 01:09:14 +0530

    Create set5.10.c

---
+++ set5.10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+   int n,flag=0;
+   scanf("%d",&n);
+
+   while(n!=0)
+   {
+       if((n%2)!=0)
+       {
+           flag=1;
+           break;
+       }
+      n=n/2; 
+   }
+
+if(flag==0)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+    return 0;
+}
