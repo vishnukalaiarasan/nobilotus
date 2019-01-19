271026d9f9b498be40e220bca20f08780fbf049e
nobilotus <46036915+nobilotus@users.noreply.github.com>
2019-01-19 22:09:25 +0530

    Create set5.1.c

---
+++ set5.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i;
+    char a[20];
+    scanf("%s",a);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("\n%s",a);
+    }
+
+    return 0;
+}
