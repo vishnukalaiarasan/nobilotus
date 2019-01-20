345c784ab7643b076c20345a56ae68689bdfa87c
nobilotus <46036915+nobilotus@users.noreply.github.com>
2019-01-20 21:20:33 +0530

    Create set6.5.c

---
+++ set6.5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,sum;
+    scanf("%d%d",&num1,&num2);
+    sum=num1+num2;
+    if(sum%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+    return 0;
+}
