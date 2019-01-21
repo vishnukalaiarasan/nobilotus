4230e1fd720c12e767666deb18f43502a150a222
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-01-21 22:25:46 +0530

    Create set7.8.c

---
+++ set7.8.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int num;
+   scanf("%d",&num);
+   if(num%7==0)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+    return 0;
+}
