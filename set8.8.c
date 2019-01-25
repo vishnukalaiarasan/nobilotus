4ac073e278c78402b785d30266e1e49e5ea6f545
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-01-25 22:36:17 +0530

    Create set8.8.c

---
+++ set8.8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,i;
+    scanf("%d",&num);
+    if(num%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("not");
+    }
+    
+    return 0;
+}
